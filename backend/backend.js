912d9092f687c56ba140cb38fef8baa2abc6fa2a
Ben Piperno <brpiperno@gmail.com>
2026-02-13 11:08:59 -0500

    started working on backend connections

--- backend/backend.js
+++ backend/backend.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import logRouter from './routes/log.js'
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.static('frontend'));
+
+app.use("/api/", logRouter);
 
 app.listen(PORT, () => {
   console.log("Server running in port ", PORT);
---
+++ frontend/js/post-log.js
@@ -0,0 +1,49 @@
+//TIPP session logging functionality
+
+console.log("Hello from frontend!");
+
+function GetLogs() {
+  //get all logs in a given time frame
+
+  const me = {};
+
+  me.showError = ( {msg = "", res, type = "danger"} = {}) => {
+    const main = document.querySelector("main");
+    const alert = document.createElement("div");
+    alert.className = `alert alert-${type}`;
+    alert.role = type;
+    alert.innerText = `${msg}: ${res.status} ${res.statusText}`;
+    main.prepend(alert);
+  };
+
+  me.renderLogs = (logs) => {
+    console.log("renderLogs:", logs[0])
+    const logDiv = document.getElementById("logs");
+    logDiv.innerHTML = ""; //clear out any old cards
+    for (const {emotionBefore, emotionAfter} of logs) {
+      const card = document.createElement("div");
+      card.className = "card mb-3";
+      card.innerHTML = `
+        <div>${emotionBefore} ${emotionAfter} </div> 
+      `;
+      logDiv.appendChild(card);
+    }
+  };
+
+  me.refreshLogs = async () => {
+    const res = await fetch("/api/log");
+    if (!res.ok) {
+      console.error("Failed to fetch log data");
+      me.showError( {msg: "Failed to load log data. Please try again later.", res});
+      return;
+    }
+
+    const data = await res.json();
+    console.log("Fetched logs", data);
+    me.renderLogs(data.logs);
+  };
+  return me;
+}
+
+const logs = GetLogs();
+logs.refreshLogs();
