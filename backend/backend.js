18be1f9308f2541207571c7fff162723ee1df6c7
Ben Piperno <103087542+brpiperno@users.noreply.github.com>
2026-02-19 22:36:24 -0500

    added in error handling if loading env file fails (in production)

--- backend/backend.js
+++ backend/backend.js
@@ -1,21 +1,25 @@
-import express from 'express';
-import sessionLogRouter from './routes/sessionLogRouter.js'
-import authRouter from './routes/authRouter.js'
+import express from "express";
+import sessionLogRouter from "./routes/sessionLogRouter.js";
+import authRouter from "./routes/authRouter.js";
 
-process.loadEnvFile();
+try {
+  process.loadEnvFile();
+} catch {
+  console.log("Unable to load env file. Not needed if this is in production!");
+}
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(express.static('frontend'));
+app.use(express.static("frontend"));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/", sessionLogRouter);
-app.use("/api/auth", authRouter)
+app.use("/api/auth", authRouter);
 app.use("/api/log", sessionLogRouter);
 
 app.listen(PORT, () => {
   console.log("Server running in port ", PORT);
-})
+});
